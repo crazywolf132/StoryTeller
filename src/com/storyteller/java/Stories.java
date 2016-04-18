0ff050c51f78982e7fa4633a793055330b843ed9
Lord Fendrel <FendrelTheLord@yandex.com>
2016-04-18 13:22:05 +1000

    WASSSSSUP!

---
+++ src/com/storyteller/java/Stories.java
@@ -0,0 +1,16 @@
+package com.storyteller.java;
+
+/**
+ * Created by travis.henley on 18/04/2016.
+ */
+public class Stories {
+
+    public Stories() {
+        ;
+    }
+
+    public String generateStory() {
+        ;
+    }
+
+}
