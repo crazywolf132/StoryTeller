a63853eecd87c1ee73187a3796365c8bb2a87deb
Lord Fendrel <FendrelTheLord@yandex.com>
2016-04-18 13:51:34 +1000

    story.txt url

--- src/com/storyteller/java/Stories.java
+++ src/com/storyteller/java/Stories.java
@@ -1,6 +1,10 @@
 package com.storyteller.java;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.io.File;
 
 /**
  * Created by travis.henley on 18/04/2016.
@@ -14,7 +18,15 @@ public class Stories {
     public String generateStory(String char1) {
         //story.txt
         URL url = Stories.class.getClassLoader().getResource("story.txt");
-        return null;
+        String text = null;
+        try {
+            text = new String(Files.readAllBytes(new File(url.toURI()).toPath()), "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return text.replace("$char$", char1);
     }
 
 }
